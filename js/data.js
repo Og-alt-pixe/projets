6332966e8777459c049feafe511e162edddf7c18
Og-alt-pixe <aliceauger@hotmail.com>
2026-06-29 04:03:56 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,184 @@
+
+
+
+//------------VOLATILES-------------->
+const PROJECTS_DATA = [
+    {
+        id: "volatiles-paris",
+        title: "L'empreinte \nd'un geste",
+        category: "événement signature",
+        displayCategory: "événement artisanal",
+        year: "2025",
+        type: "ARTISANAT & événement",
+        discipline: "DESIGN & ARTISANAT",
+        description: `RENCONTRE ENTRE L'UNIVERS DU VIN ET LA PERSONNALISATION ARTISANALE.
+
+En collaboration avec la cave à vin Volatiles, j'ai imaginé et réalisé une série d'animations de gravure personnalisée sur différents supports : verres, bouteilles, carafes et sous-verres. De la recherche graphique à la création de motifs sur mesure, en passant par la communication de l'événement, j'ai pris en charge l'ensemble du projet.
+
+Chaque objet a nécessité une phase d'expérimentation afin d'adapter le geste de gravure à ses contraintes et spécificités d’usage. Cette collaboration a été l'occasion d'explorer la place de l'artisanat et de la personnalisation dans l'univers du vin, tout en créant un moment d'échange privilégié avec le public.
+`,
+        imageHero: "image/volatiles2.webp",
+        imageNext: "image/lucie-logo2.webp",
+        url: "projet-page.html?id=volatiles-paris",
+        gallery: [
+            { src: "image/store.webp", layout: "" },
+            { src: "image/noel.webp", layout: "" },
+            { src: "image/croquis-2.webp", layout: "landscape" },
+            { src: "image/grav.mp4", layout: "portrait", type: "video" },
+            { src: "image/2025-giro-mains-cave-large.png", layout: "" },
+            { src: "image/volatiles0.webp", layout: "portrait" },
+            { src: "image/raisin.webp", layout: "square" },
+            { src: "image/2025-verre-raisin-couleur-anim-semilarge.png", layout: "" },
+
+            { src: "image/volatiles2.webp", layout: "" }
+
+
+
+        ]
+    },
+
+    //------------LUCIE-------------->
+    {
+        id: "lucie-allard",
+        title: "Lucie Allard Design",
+        category: "graphisme",
+        displayCategory: "Eco-design",
+        year: "2022-2024",
+        type: "Graphisme",
+        discipline: "Identité visuelle",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/lucie-logo2.webp",
+        imageNext: "image/client-logo(2).webp",
+        url: "projet-page.html?id=lucie-allard",
+        gallery: [ 
+            { src: "image/projets/lucie/logo.webp", layout: "square" },
+             { src: "image/projets/lucie/gif.gif", layout: "portrait", type: "gif" },
+           
+        ]
+    },
+
+    //------------CARAFE-------------->
+    {
+        id: "thuizat-carafe",
+        title: "Gravure Carafe Riedel",
+        category: "artisanat",
+        displayCategory: "Gravure sur cristal",
+        year: "2025",
+        type: "Artisanat",
+        discipline: "Design & Artisanat",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/client-logo(2).webp",
+        imageNext: "image/jaaj.webp",
+        url: "projet-page.html?id=thuizat-carafe",
+        gallery: [
+            { src: "image/client-logo(2).webp", layout: "" },
+            { src: "image/client-logo(1).webp", layout: "" },
+            { src: "image/client-calli(2).webp", layout: "square" }
+        ]
+    },
+
+
+    //------------JAAJ-------------->
+    {
+        id: "jaaj-ttf",
+        title: "JAAJ.TTF",
+        category: "graphisme",
+        displayCategory: "Font design",
+        year: "2020",
+        type: "Graphisme",
+        discipline: "Design Typographique",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/jaaj.webp",
+        imageNext: "image/client-illu(4).webp",
+        url: "projet-page.html?id=jaaj-ttf",
+        gallery: [
+            { src: "image/jaaj.webp", layout: "landscape" },
+            { src: "image/jaaj4.webp", layout: "" },
+            { src: "image/jaaj4.webp", layout: "" }
+
+        ]
+    },
+
+    //------------PERSO-------------->
+    {
+        id: "perso",
+        title: "Design, Motifs & Gravure",
+        category: "artisanat",
+        displayCategory: "Design & Artisanat",
+        year: "depuis 2024",
+        type: "Artisanat",
+        discipline: "Design artisanale",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/client-illu(4).webp",
+        imageNext: "image/home/id.webp",
+        url: "projet-page.html?id=perso",
+        gallery: [
+            { src: "image/client-illu(4).webp", layout: "portrait" },
+            { src: "image/client-illu(1).webp", layout: "" },
+            { src: "image/raisin.webp", layout: "" }
+
+        ]
+    },
+
+    //------------LIRE-------------->
+    {
+        id: "memoire",
+        title: "I DO.",
+        category: "graphisme",
+        displayCategory: "Expérience utilisateur",
+        year: "209-2021",
+        type: "Graphisme",
+        discipline: "Design graphique & numérique",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/home/id.webp",
+        imageNext: "image/home/archi2.webp",
+        url: "projet-page.html?id=memoire",
+        gallery: [
+            { src: "image/lire.webp", layout: "square" },
+            { src: "image/lire.webp", layout: "square" }
+
+        ]
+    },
+
+    //------------HIBOU-------------->
+    {
+        id: "archi-lucas",
+        title: "L'Archimede",
+        category: "graphisme",
+        displayCategory: "Poster Illustré",
+        year: "2024",
+        type: "Graphisme",
+        discipline: "Graphisme et Illustration",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/home/archi2.webp",
+        imageNext: "image/home/client-calli(1).webp",
+        url: "projet-page.html?id=archi-lucas",
+        gallery: [
+            { src: "image/perso-archi(1).webp", layout: "landscape" },
+            { src: "image/archi3.webp", layout: "" },
+            { src: "image/perso-archi(2).webp", layout: "landscape" }
+
+        ]
+    },
+
+    //------------LVMH-------------->
+    {
+        id: "lvmh",
+        title: "Gravure à Paris",
+        category: "artisanat",
+        displayCategory: "Prestation artisanale",
+        year: "depuis 2029",
+        type: "Animation gravure",
+        discipline: "Gravure artisanale",
+        description: "Accompagnement de Lucie Allard dans sa nouvelle image...",
+        imageHero: "image/home/anim-calli(1).webp",
+        imageNext: "",
+        url: "projet-page.html?id=lvmh",
+        gallery: [
+            { src: "image/anim-baton(1).webp", layout: "" },
+            { src: "image/home/anim-calli(1).webp", layout: "" },
+            { src: "image/anim-calli(2).webp", layout: "portrait" }
+
+        ]
+    }
+];
